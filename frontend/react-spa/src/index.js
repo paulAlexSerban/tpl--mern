ba904b7681e1c3ff56630bc01d7c5b81a99823a2
paulAlexSerban <paul.alex.serban@gmail.com>
2023-04-29 11:30:09 +0300

    feat(modal): added initial modal implementation

--- frontend/react-spa/src/index.js
+++ frontend/react-spa/src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
-
 import './index.scss';
 import App from './App';
-
-
 
 import { createRoot } from 'react-dom/client';
 
 // Clear the existing HTML content
-document.body.innerHTML = '<div id="drawer-hook"></div><div id="backdrop-hook"></div><div id="root"></div>';
+document.body.innerHTML =
+    `<div id="drawer-hook"></div>
+    <div id="modal-hook"></div>
+    <div id="backdrop-hook"></div>
+    <div id="root"></div>`;
 
 // Render your React component instead
 const root = createRoot(document.getElementById('root'));
