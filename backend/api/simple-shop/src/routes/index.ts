6b50925bfdff2701a922bf62785268c25cb64387
paulAlexSerban <paul.alex.serban@gmail.com>
2024-02-01 16:45:16 +0200

    feat(simple-shop): added init directories

---
+++ backend/api/simple-shop/src/routes/index.ts
@@ -0,0 +1,13 @@
+import express, { Router, Request, Response, NextFunction } from 'express';
+const router: Router = express.Router();
+
+/* GET */
+router.get('/', function (req: Request, res: Response, next: NextFunction) {
+    res.send(
+        JSON.stringify({
+            name: 'simple-shop',
+        })
+    );
+});
+
+export default router;
