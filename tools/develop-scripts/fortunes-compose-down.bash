c96df1fc64663f16ac5eb39051511fb7026d7338
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-12 23:07:57 +0200

    feat(fortunes): added

---
+++ tools/develop-scripts/fortunes-compose-down.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+# PROJECT_NAME=$(node -p "require('../../package.json').formattedName.split('/').pop()")
+# PROJECT_VERSION=$(node -p "require('../../package.json').version")
+
+# echo "[ 🛑 🐳 compose down ] $PROJECT_NAME:$PROJECT_VERSION"
+
+docker compose --env-file ../../shared/config/fortunes.compose.env \
+  --file ../../infrastructure/docker/docker-compose.fortunes.dev.yml \
+  down --volumes --rmi all
