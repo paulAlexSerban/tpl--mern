f0db58a36c9943a425d4a9fff2dae2ad9d8adfa8
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-14 21:11:35 +0200

    feat(monsters api and sql): added api and database

---
+++ tools/develop-scripts/monsters-compose-down.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+# PROJECT_NAME=$(node -p "require('../../package.json').formattedName.split('/').pop()")
+# PROJECT_VERSION=$(node -p "require('../../package.json').version")
+
+# echo "[ 🛑 🐳 compose down ] $PROJECT_NAME:$PROJECT_VERSION"
+
+docker compose --env-file ../../shared/config/monsters.compose.env \
+  --file ../../infrastructure/docker/docker-compose.monsters.dev.yml \
+  down --volumes --rmi all
