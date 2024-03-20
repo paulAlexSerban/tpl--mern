a1bf4bb1373d59a4079441070a6218d702174abb
paulAlexSerban <paul.alex.serban@gmail.com>
2024-03-20 18:55:06 +0200

    refactor(cleanup): remove deprecated code

--- _DEPRECATED/frontend/react-spa/scripts/start.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# makes sure the folder containing the script will be the root folder
-cd "$(dirname "$0")" || exit
-
-source ../config.env
-
-npm --prefix .. run start
--- _DEPRECATED/wiki/folserst-structure.md
+++
@@ -1,5 +0,0 @@
-# Folder structure
-
-## frontend/react-spa
-
-- split by feature with one folders that keeps the shared components between features
