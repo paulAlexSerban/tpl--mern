5651ba1354f51cef3446eae80cde0a66669b133d
Paul Serban <paul.alex.serban@gmail.com>
2024-04-04 19:18:00 +0300

    feat(events-showcase): full local infrastructure setup

---
+++ tools/develop-scripts/events-showcase-compose-up.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+# PROJECT_NAME=$(node -p "require('../../package.json').formattedName.split('/').pop()")
+# PROJECT_VERSION=$(node -p "require('../../package.json').version")
+
+# echo "[ 🟢 🐳 compose up ] $PROJECT_NAME:$PROJECT_VERSION"
+
+docker compose --env-file ../../infrastructure/env/events-showcase.compose.env \
+  --file ../../infrastructure/docker/docker-compose.events-showcase.dev.yml \
+  up --detach --build
