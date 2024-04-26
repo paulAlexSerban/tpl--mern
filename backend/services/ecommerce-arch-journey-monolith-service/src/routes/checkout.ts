6f534f1d8cabe349f45da1e3d2707a7ef448baf9
paulAlexSerban <paul.alex.serban@gmail.com>
2024-04-26 15:04:30 +0300

    feat(ecomm-arch-journey-monolith): init setup

--- backend/services/ecommerce-arch-journey-monolith-service/src/routes/checkout.ts
+++ backend/services/ecommerce-arch-journey-monolith-service/src/routes/checkout.ts
@@ -1,17 +1,13 @@
 import express, { Router, Request, Response, NextFunction } from 'express';
 const router: Router = express.Router();
-
-import { products } from './admin';
+import { cart } from '../data';
 
 /* GET */
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true,
+    res.render('checkout', {
+        pageTitle: 'Checkout',
+        path: '/checkout',
+        cart,
     });
 });
 
--- backend/services/ecommerce-arch-journey-monolith-service/src/routes/privacy.ts
+++ backend/services/ecommerce-arch-journey-monolith-service/src/routes/privacy.ts
@@ -1,17 +1,11 @@
 import express, { Router, Request, Response, NextFunction } from 'express';
 const router: Router = express.Router();
 
-import { products } from './admin';
-
 /* GET */
 router.get('/', function (req: Request, res: Response, next: NextFunction) {
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true,
+    res.render('privacy', {
+        pageTitle: 'Privacy',
+        path: '/privacy',
     });
 });
 
