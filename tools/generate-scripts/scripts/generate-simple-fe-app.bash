b8d7d37feb4d14eac2e4a7e85c17af9b07f090c7
paulAlexSerban <paul.alex.serban@gmail.com>
2024-05-14 00:23:07 +0300

    feat(generaotr): add error handling and script to yarn

--- tools/generate-scripts/scripts/generate-simple-fe-app.bash
+++ tools/generate-scripts/scripts/generate-simple-fe-app.bash
@@ -2,8 +2,14 @@
 # makes sure the folder containing the script will be the root folder
 cd "$(dirname "$0")" || exit
 
-export NAME_SLUG='test'
-export NAME_FORMATTED='Test App'
+if [ "$#" -ne 2 ]; then
+  echo "You need to provide the name of the app as a slug and as a formatted name"
+  echo "Example: ./generate-simple-fe-app.bash simple-fe-app 'Simple FE App'"
+  exit 1
+fi
+
+export NAME_SLUG=$1
+export NAME_FORMATTED=$2
 
 bash ./_frontend_apps_vite-react-ts-app.bash
 bash ./_backend_middleware_simple-proxy.bash
