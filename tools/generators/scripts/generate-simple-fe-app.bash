78c88fc08c6cca5a899fdb511173a49a8446a307
paulAlexSerban <paul.alex.serban@gmail.com>
2024-05-14 00:14:13 +0300

    feat(generator): develop script

--- tools/generators/scripts/generate-simple-fe-app.bash
+++ tools/generators/scripts/generate-simple-fe-app.bash
@@ -9,3 +9,4 @@ bash ./_frontend_apps_vite-react-ts-app.bash
 bash ./_backend_middleware_simple-proxy.bash
 bash ./_infrastructure_docker-simple-fe-setup.bash
 bash ./_infrastructure_env-simple-env.bash
+bash ./_tools_develop-scripts.bash
