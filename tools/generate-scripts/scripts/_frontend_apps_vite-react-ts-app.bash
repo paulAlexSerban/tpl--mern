ab44886fe925d556cc1fc85e4ef138af238499ed
paulAlexSerban <paul.alex.serban@gmail.com>
2024-05-14 09:46:07 +0300

    fix(generator): adjust copy and rename scripts

--- tools/generate-scripts/scripts/_frontend_apps_vite-react-ts-app.bash
+++ tools/generate-scripts/scripts/_frontend_apps_vite-react-ts-app.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 # makes sure the folder containing the script will be the root folder
 cd "$(dirname "$0")" || exit
-
-# copy content of template folder to ../../../frontend/apps/$NAME_SLUG folder
+# create folder ../../../frontend/apps/$NAME_SLUG folder
 mkdir -p ../../../frontend/apps/${NAME_SLUG}-app
+# copy content of template folder to ../../../frontend/apps/$NAME_SLUG folder
 cp -rfv ../_templates/frontend_apps_vite-react-ts-app/* ../../../frontend/apps/$NAME_SLUG-app/
 # search and replace {{frontend_apps_vite-react-ts-app-slug}} with the name of the frontend app
 find ../../../frontend/apps/${NAME_SLUG}-app -type f -exec sed -i '' -e "s/{{frontend_apps_vite-react-ts-app-slug}}/${NAME_SLUG}-app/g" {} \;
