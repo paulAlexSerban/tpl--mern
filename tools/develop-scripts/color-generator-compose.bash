0ca9248ac18292bc4eaf068ce1535eef9ddbbff3
paulAlexSerban <paul.alex.serban@gmail.com>
2024-05-16 00:17:44 +0300

    feat(color-generator): initial implementation

---
+++ tools/develop-scripts/color-generator-compose.bash
@@ -0,0 +1,37 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+export HOST_USER_ID=$(id -u)
+export HOST_GROUP_ID=$(id -g)
+
+function up() {
+    echo "[ 🟢 🐳 compose up ]"
+    docker compose --env-file ../../infrastructure/env/color-generator.compose.env \
+    --file ../../infrastructure/docker/docker-compose.color-generator.dev.yml \
+    up --detach --build
+    docker compose --env-file ../../infrastructure/env/color-generator.compose.env \
+    ps
+}
+
+function down() {
+    echo "[ 🛑 🐳 compose down ]"
+    docker compose --env-file ../../infrastructure/env/color-generator.compose.env \
+    --file ../../infrastructure/docker/docker-compose.color-generator.dev.yml \
+    down --volumes --rmi all
+    docker compose --env-file ../../infrastructure/env/color-generator.compose.env \
+    ps
+}
+
+function logs() {
+    echo "[ 📜 🐳 compose logs ]"
+    docker compose --env-file ../../infrastructure/env/color-generator.compose.env \
+    --file ../../infrastructure/docker/docker-compose.color-generator.dev.yml \
+    logs --follow
+}
+
+function help() {
+    echo "Usage: $0 {up|down|logs}"
+}
+
+$1
