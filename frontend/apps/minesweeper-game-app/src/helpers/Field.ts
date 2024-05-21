1222dd440db1d9f5c63eb660fd61711cfb3f9521
paulAlexSerban <paul.alex.serban@gmail.com>
2024-05-21 21:49:15 +0300

    feat(minesweeper-game): field and cell manipulator

--- frontend/apps/minesweeper-game-app/src/helpers/Field.ts
+++ frontend/apps/minesweeper-game-app/src/helpers/Field.ts
@@ -1,3 +1,5 @@
+import { incrementNeighbors } from './CellManipulators';
+
 export type Cell = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 export type Field = Cell[][];
 export type Coords = [number, number];
@@ -10,15 +12,32 @@ export const CellState: Record<string, Cell> = {
     weakMark: 12,
 };
 
-export const fieldExample: Field = [
-    [9, 1, 0, 0, 0, 0, 0, 1, 1, 1],
-    [3, 3, 1, 0, 0, 0, 0, 1, 9, 1],
-    [0, 0, 0, 0, 0, 0, 0, 1, 2, 2],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 9],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [9, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [1, 2, 2, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 9, 1, 0, 0, 0, 0, 0, 0],
-];
+export const emptyFieldGenerator = (size: number, state: Cell = CellState.empty): Field =>
+    new Array(size).fill(null).map(() => new Array(size).fill(state));
+
+export const fieldGenerator = (size: number, probability: number): Field => {
+    if (probability < 0 || probability > 1) {
+        throw new Error('Probability must be between 0 and 1');
+    }
+
+    let unprocessedCells = size * size;
+    let restCellsWithBombs = unprocessedCells * probability;
+
+    const result: Field = emptyFieldGenerator(size);
+
+    for (let y = 0; y < size; y++) {
+        for (let x = 0; x < size; x++) {
+            if (restCellsWithBombs === 0) {
+                return result;
+            }
+            if (restCellsWithBombs / unprocessedCells > Math.random()) {
+                result[y][x] = CellState.bomb;
+                incrementNeighbors([y, x], result);
+                restCellsWithBombs--;
+            }
+            unprocessedCells--;
+        }
+    }
+
+    return result;
+};
