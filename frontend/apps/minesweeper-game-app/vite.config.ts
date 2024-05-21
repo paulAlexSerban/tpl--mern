9c7a629aecd37a97b3e8048f1436cee75bfa1dcb
paulAlexSerban <paul.alex.serban@gmail.com>
2024-05-21 20:20:46 +0300

    feat(minesweeper game): improove setup

---
+++ frontend/apps/minesweeper-game-app/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+    const config = {
+        plugins: [react()],
+        base: '/',
+    };
+
+    return config;
+});
