bf4d44be09f5ee0e33b6bee0c94498f7f5f87290
paulAlexSerban <paul.alex.serban@gmail.com>
2024-06-30 16:07:29 +0300

    feat(megamenu-strapi-clone): init implementation

--- frontend/apps/megamenu-strapi-clone-app/src/App.tsx
+++ frontend/apps/megamenu-strapi-clone-app/src/App.tsx
@@ -1,30 +1,14 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Hero from './components/Hero';
+import Sidebar from './components/Sidebar';
+import MegaMenu from './components/MegaMenu/MegaMenu';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Megamenu Strapi Clone</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        <main>
+            <MegaMenu />
+            <Hero />
+            <Sidebar />
+        </main>
     );
 }
 
