7797798320ccf4fbcc26684371a036922852a22c
paulAlexSerban <paul.alex.serban@gmail.com>
2024-07-05 09:55:14 +0300

    feat(shoppping-cart-usereducer): init implementation

---
+++ tools/develop-scripts/shopping-cart-usereducer-compose.bash
@@ -0,0 +1,67 @@
+#!/bin/bash
+# makes sure the folder containing the script will be the root folder
+cd "$(dirname "$0")" || exit
+
+export HOST_USER_ID=$(id -u)
+export HOST_GROUP_ID=$(id -g)
+
+ENV_FILE="../../infrastructure/env/shopping-cart-usereducer.compose.env"
+COMPOSE_FILE_DEV="../../infrastructure/docker/docker-compose.shopping-cart-usereducer.dev.yml"
+COMPOSE_FILE_PROD="../../infrastructure/docker/docker-compose.shopping-cart-usereducer.prod.yml"
+
+function list() {
+    echo "[ 📜 🐳 compose list ]"
+    docker compose \
+        --env-file ${ENV_FILE} \
+        --file ${COMPOSE_FILE_DEV} ps
+}
+
+function up() {
+    echo "[ 🟢 🐳 compose up ]"
+    docker compose \
+        --env-file ${ENV_FILE} \
+        --file ${COMPOSE_FILE_DEV} up \
+        --detach --build
+    list
+}
+
+function down() {
+    echo "[ 🛑 🐳 compose down ]"
+    docker compose \
+        --env-file ${ENV_FILE} \
+        --file ${COMPOSE_FILE_DEV} down \
+        --volumes --rmi all
+    list
+}
+
+function logs() {
+    echo "[ 📜 🐳 compose logs ]"
+    docker compose \
+        --env-file ${ENV_FILE} \
+        --file ${COMPOSE_FILE_DEV} logs \
+        --follow
+}
+
+function up-prod() {
+    echo "[ 🟢 🐳 compose up production build ]"
+    docker compose \
+        --env-file ${ENV_FILE} \
+        --file ${COMPOSE_FILE_PROD} up \
+        --detach --build --wait
+    list
+}
+
+function down-prod() {
+    echo "[ 🛑 🐳 compose down production build ]"
+    docker compose \
+        --env-file ${ENV_FILE} \
+        --file ${COMPOSE_FILE_PROD} down \
+        --volumes --rmi all
+    list
+}
+
+function help() {
+    echo "Usage: $0 {up|down|logs}"
+}
+
+$1
