2c78e00a0d2cb8ef2759be82101774c996c3ef17
paulAlexSerban <paul.alex.serban@gmail.com>
2024-07-29 10:41:49 +0300

    refactor(package script): commnet possibly broken code

--- frontend/sites/flowrise-site/scripts/package.bash
+++ frontend/sites/flowrise-site/scripts/package.bash
@@ -6,6 +6,6 @@ if [[ -d "../dist" ]]; then
   #  get project name from package.json
   PROJECT_NAME=$(node -p "require('../package.json').name.split('/').pop()")
   echo "📦  Package $PROJECT_NAME"
-  mkdir -p ../../../../package/apps/$PROJECT_NAME
-  cp -rfv ../dist/* ../../../../package/apps/$PROJECT_NAME
+  # mkdir -p ../../../../package/apps/$PROJECT_NAME
+  # cp -rfv ../dist/* ../../../../package/apps/$PROJECT_NAME
 fi
