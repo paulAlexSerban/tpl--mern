4b400587c37a6cf5d7cf9fde16b10334ada7257f
paulAlexSerban <paul.alex.serban@gmail.com>
2024-08-18 13:23:25 +0300

    feat(redux-shopping-cart-v2): init

---
+++ frontend/apps/redux-shopping-cart-v2/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <>
+            <h1>Redux Shopping cart</h1>
+        </>
+    );
+};
+
+export default App;
---
+++ frontend/apps/redux-shopping-cart-v2/src/store/index.ts
@@ -0,0 +1,23 @@
+import { configureStore, Store } from '@reduxjs/toolkit';
+
+/**
+ * Creates and configures the Redux store.
+ * The store is created with a root reducer consisting of `unitReducer` and `statusSlice`.
+ * @NOTE: The configureStore function is designed to infer types automatically, so you typically don't need to manually specify types with Store
+ */
+export const store: Store = configureStore({
+    reducer: {},
+});
+
+/**
+ * Infers and exports the `RootState` type from the store's state.
+ * The `RootState` type represents the complete state of the Redux store.
+ */
+export type RootState = ReturnType<typeof store.getState>;
+
+/**
+ * Infers and exports the `AppDispatch` type from the store's dispatch function.
+ * The `AppDispatch` type represents the dispatch function to interact with the Redux store.
+ */
+export type AppDispatch = typeof store.dispatch;
+export type AppStore = typeof store;
