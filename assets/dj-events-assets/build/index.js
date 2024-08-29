9c594a78fcb477f84398f99bdc64ad713b3c1d05
paulAlexSerban <paul.alex.serban@gmail.com>
2024-08-29 09:34:45 +0300

    feat(dj-events): added static assets service

---
+++ assets/dj-events-assets/build/index.js
@@ -0,0 +1,22 @@
+import { task, parallel, series } from 'gulp';
+import { clean } from './tasks/clean';
+import { processIcons } from './tasks/processIcons';
+import { processSvgs } from './tasks/processSvgs';
+import { createImageRenditions } from './tasks/createImageRenditions';
+import { processVideos } from './tasks/processVideos';
+import { processAudio } from './tasks/processAudio';
+import { processGifs } from './tasks/processGifs';
+
+// ---------------------------------------------------------------------
+// | Helper tasks                                                      |
+// ---------------------------------------------------------------------
+
+// ---------------------------------------------------------------------
+// | Main tasks                                                        |
+// ---------------------------------------------------------------------
+task(
+    'process',
+    series(clean, parallel(processIcons, processSvgs, createImageRenditions, processVideos, processAudio, processGifs))
+);
+
+task();
