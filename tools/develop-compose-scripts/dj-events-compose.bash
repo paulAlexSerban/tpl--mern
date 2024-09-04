f628705395a0a51416b2230b3290b54bcd3a16d7
paulAlexSerban <paul.alex.serban@gmail.com>
2024-09-04 13:06:01 +0300

    feat(dj-events): add database backup script

--- tools/develop-compose-scripts/dj-events-compose.bash
+++ tools/develop-compose-scripts/dj-events-compose.bash
@@ -7,6 +7,9 @@ export HOST_GROUP_ID=$(id -g)
 
 ENV_FILE="../../infrastructure/env/dj-events.compose.env"
 COMPOSE_FILE_DEV="../../infrastructure/docker/dev/docker-compose.dj-events.dev.yml"
+DATABASE_BACKUP_FOLDER="../../database/seeds/dj-events-db"
+
+source $ENV_FILE
 
 function list() {
     echo "[ 📜 🐳 compose list ]"
@@ -15,12 +18,40 @@ function list() {
         --file ${COMPOSE_FILE_DEV} ps
 }
 
+function backup-database() {
+    echo "[ 🔄 🐳 compose backup database ]"
+    # if [ -f "${DATABASE_BACKUP_FOLDER}/dj-events-backup.sql" ]; then
+    #     echo "[ info ] Backup exists, copying to old folder"
+    #     mv ${DATABASE_BACKUP_FOLDER}/dj-events-backup.sql ${DATABASE_BACKUP_FOLDER}/dj-events-backup-$(date +%F_%H-%M-%S).sql
+    # fi
+    docker exec dj-events_mariadb-database-service mariadb-dump -u${DATABASE_USERNAME} -p${DATABASE_PASSWORD} ${DATABASE_NAME} >${DATABASE_BACKUP_FOLDER}/dj-events-backup.sql
+}
+
+function restore-database() {
+    echo "[ 🔄 🐳 compose restore database ]"
+
+    # check if database service is available if not retry 3 times with 5 seconds interval
+    for i in {1..3}; do
+        echo "[ info ] Checking if database service is available"
+        docker exec dj-events_mariadb-database-service mariadb -u${DATABASE_USERNAME} -p${DATABASE_PASSWORD} ${DATABASE_NAME} -e "SELECT 1" && break
+        sleep 5
+    done
+
+    if [ -f "${DATABASE_BACKUP_FOLDER}/dj-events-backup.sql" ]; then
+        echo "[ info ] Restoring database from backup"
+        docker exec -i dj-events_mariadb-database-service mariadb -u${DATABASE_USERNAME} -p${DATABASE_PASSWORD} ${DATABASE_NAME} <${DATABASE_BACKUP_FOLDER}/dj-events-backup.sql
+    else
+        echo "[ ❌ error ] No backup file found"
+    fi
+}
+
 function up() {
     echo "[ 🟢 🐳 compose up ]"
     docker compose \
         --env-file ${ENV_FILE} \
         --file ${COMPOSE_FILE_DEV} up \
         --detach --build
+    restore-database
     list
 }
 
@@ -42,8 +73,11 @@ function recreate-strapi-cms() {
     list
 }
 
+
+
 function down() {
     echo "[ 🛑 🐳 compose down ]"
+    backup-database
     docker compose \
         --env-file ${ENV_FILE} \
         --file ${COMPOSE_FILE_DEV} down \
