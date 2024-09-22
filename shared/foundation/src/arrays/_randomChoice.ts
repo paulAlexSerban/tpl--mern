fef585f8b82d578db24e820fb2c34e6f3a70d532
paulAlexSerban <paul.alex.serban@gmail.com>
2024-09-22 22:36:53 +0300

    feat(foundation): small adjustments

--- shared/foundation/src/arrays/_randomChoice.ts
+++ shared/foundation/src/arrays/_randomChoice.ts
@@ -1,9 +1,9 @@
 /**
  * Returns a random element from the given array.
- * @param {Array} arr[] - The array to choose from.
- * @returns
+ * @param {T[]} arr - The array to choose from.
+ * @returns {T} A random element from the array.
  */
-const randomChoice = (arr) => {
+const randomChoice = <T>(arr: T[]): T => {
     let randomIndex = Math.floor(Math.random() * arr.length);
     return arr[randomIndex];
 };
--- shared/foundation/src/numbers/_padToThree.ts
+++ shared/foundation/src/numbers/_padToThree.ts
@@ -1,8 +1,10 @@
 /**
  * padToThree - adds 0s to the front of a number to make it 3 digits long
  * @param {*} number
- * @returns
+ * @returns {string | number} - the number as a string with 0s added to the front
  */
-const padToThree = (number: number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+const padToThree = (number: number): string | number => {
+    return number <= 999 ? `00${number}`.slice(-3) : number;
+};
 
 export default padToThree;
