e8618e5be8e000714669ee7944ef73da444d081d
paulAlexSerban <paul.alex.serban@gmail.com>
2024-10-06 13:14:20 +0300

    feat(dj-events): add events routes

---
+++ backend/services/dj-events-ssr-service/src/pages/api/events/events.d.ts
@@ -0,0 +1,14 @@
+export type Event = {
+    id: string;
+    name: string;
+    slug: string;
+    venue: string;
+    address: string;
+    performers: string;
+    date: string;
+    time: string;
+    description: string;
+    image: string;
+};
+
+export type EventsResponseData = Event[];
