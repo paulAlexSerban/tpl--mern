38ff97b94257f0afd9e0fa6db7692544b3066436
paulAlexSerban <paul.alex.serban@gmail.com>
2024-10-14 09:54:43 +0300

    fix(dj-events): strapi querires

--- backend/services/dj-events-ssr-service/src/types.d.ts
+++ backend/services/dj-events-ssr-service/src/types.d.ts
@@ -1,3 +1,30 @@
+type EventDescription = [
+    {
+        type: string;
+        children: [
+            {
+                type: string;
+                text: string;
+            },
+        ];
+    },
+];
+
+type EventImage = {
+    data: {
+        attributes: {
+            name: string;
+            alternativeText: string;
+            caption: string;
+            formats: {
+                [key in ImageFormats]: {
+                    url: string;
+                };
+            };
+        };
+    };
+}
+
 type EventAttributes = {
     name: string;
     slug: string;
@@ -6,33 +33,10 @@ type EventAttributes = {
     performers: string;
     date: string;
     time: string;
-    description: [
-        {
-            type: string;
-            children: [
-                {
-                    type: string;
-                    text: string;
-                },
-            ];
-        },
-    ];
-    image: {
-        data: {
-            attributes: {
-                name: string;
-                alternativeText: string;
-                caption: string;
-                formats: {
-                    [key in ImageFormats]: {
-                        url: string;
-                    };
-                };
-            };
-        };
-    };
+    description: EventDescription;
+    image: EventImage;
 };
-type ImageAttributes = 'name' | 'alternativeText' | 'caption' | 'width' | 'height' | 'formats';
+
 type ImageFormats = 'thumbnail' | 'small' | 'medium' | 'large' | 'custom';
 
 export type Event = {
