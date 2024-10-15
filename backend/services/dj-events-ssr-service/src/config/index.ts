b8fa588ebe8be8a2bd601a362300abef2fe34431
paulAlexSerban <paul.alex.serban@gmail.com>
2024-10-15 22:52:30 +0300

    feat(dj-events): implemnt add events page

--- backend/services/dj-events-ssr-service/src/config/index.ts
+++ backend/services/dj-events-ssr-service/src/config/index.ts
@@ -1,4 +1,5 @@
 export const PUBLIC_APP_URL = process.env.NEXT_PUBLIC_APP_URL || `http://localhost:3000`;
 export const PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || `http://localhost:$3000/api`;
+export const PUBLIC_CMS_API_URL = `${process.env.CMS_URL}/cms` || `http://localhost:3000/cms/api`;
 
 export const PRIVATE_CMS_API_URL = process.env.PRIVATE_CMS_API_URL || `http://strapi-cms:1337/api`;
--- backend/services/dj-events-ssr-service/src/pages/events/add.tsx
+++ backend/services/dj-events-ssr-service/src/pages/events/add.tsx
@@ -1,11 +1,136 @@
 import GenericLayout from '@/components/GenericLayout';
+import { useState, FC } from 'react';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { PUBLIC_CMS_API_URL } from '@/config/index';
+import styles from '@/styles/form.module.scss';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { parseCookies } from '@/helpers/index';
+import { GetServerSideProps } from 'next';
+type AddEventPageProps = {
+    token: string;
+};
 
-const AddEventsPage = () => {
+const AddEventPage: FC = () => {
+    const [values, setValues] = useState({
+        name: '',
+        performers: '',
+        venue: '',
+        address: '',
+        date: '',
+        time: '',
+        description: '',
+    });
+
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        // Validation
+        const hasEmptyFields = Object.values(values).some((element) => element === '');
+
+        if (hasEmptyFields) {
+            toast.error('Please fill in all fields');
+        }
+
+        const res = await fetch(`${PUBLIC_CMS_API_URL}/api/events`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({ data: values }),
+        });
+
+        if (!res.ok) {
+            if (res.status === 403 || res.status === 401) {
+                toast.error('No token included');
+                return;
+            }
+            toast.error('Something Went Wrong');
+        } else {
+            const evt = await res.json();
+            const slug = evt.slug;
+            router.push(`/events/${slug}`);
+        }
+    };
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setValues({ ...values, [name]: value });
+    };
+
     return (
-        <GenericLayout>
-            <h1>Add Events</h1>
+        <GenericLayout title="Add New Event">
+            <Link href="/events">Go Back</Link>
+            <h1>Add Event</h1>
+            <ToastContainer />
+            <form onSubmit={handleSubmit} className={styles.form}>
+                <div className={styles.grid}>
+                    <div>
+                        <label htmlFor="name">Event Name</label>
+                        <input type="text" id="name" name="name" value={values.name} onChange={handleInputChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="performers">Performers</label>
+                        <input
+                            type="text"
+                            name="performers"
+                            id="performers"
+                            value={values.performers}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="venue">Venue</label>
+                        <input type="text" name="venue" id="venue" value={values.venue} onChange={handleInputChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="address">Address</label>
+                        <input
+                            type="text"
+                            name="address"
+                            id="address"
+                            value={values.address}
+                            onChange={handleInputChange}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="date">Date</label>
+                        <input type="date" name="date" id="date" value={values.date} onChange={handleInputChange} />
+                    </div>
+                    <div>
+                        <label htmlFor="time">Time</label>
+                        <input type="text" name="time" id="time" value={values.time} onChange={handleInputChange} />
+                    </div>
+                </div>
+
+                <div>
+                    <label htmlFor="description">Event Description</label>
+                    <textarea
+                        name="description"
+                        id="description"
+                        value={values.description}
+                        onChange={handleInputChange}
+                    ></textarea>
+                </div>
+
+                <input type="submit" value="Add Event" className="btn" />
+            </form>
         </GenericLayout>
     );
 };
 
-export default AddEventsPage;
+// export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+//     const { token } = parseCookies(req);
+
+//     return {
+//         props: {
+//             token,
+//         },
+//     };
+// };
+
+export default AddEventPage;
