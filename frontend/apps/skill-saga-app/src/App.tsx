6ae7b113a0efdc0b7552e9e556ce09faa8605e0e
paulAlexSerban <paul.alex.serban@gmail.com>
2024-10-27 12:41:45 +0200

    feat(skill-saga): init

---
+++ frontend/apps/skill-saga-app/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <main>
+            <h2>Skill Saga App.</h2>
+        </main>
+    );
+};
+
+export default App;
---
+++ frontend/apps/skill-saga-app/src/data/competency_types.ts
@@ -0,0 +1,27 @@
+const competency_types = [
+    {
+        id: 1,
+        name: 'Technical',
+        slug: 'technical',
+    },
+    {
+        id: 2,
+        name: 'Analytical and Problem Solving',
+        slug: 'analytical-and-problem-solving',
+    },
+    {
+        id: 3,
+        name: 'Project Management and Collaboration',
+        slug: 'project-management-and-collaboration',
+    },
+    {
+        id: 4,
+        name: 'Soft Skills',
+        slug: 'soft-skills',
+    },
+    {
+        id: 5,
+        name: 'Leadership and Mentoring',
+        slug: 'leadership-and-mentoring',
+    }
+]
